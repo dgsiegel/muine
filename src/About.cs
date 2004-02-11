67ffbe959882b1363669e95917533ab0df72266a
Jorn Baayen <jbaayen@src.gnome.org>
2004-02-11 21:49:12 +0000

    release 0.4.0

--- src/About.cs
+++ src/About.cs
@@ -22,7 +22,7 @@ using Gdk;
 
 public class About
 {
-	private static string version = "0.3.99";
+	private static string version = "0.4.0";
 	public static string Version {
 		get {
 			return version;
