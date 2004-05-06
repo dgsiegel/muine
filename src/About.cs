cdb1ebb71c4917c15fc8e629a70e290e96cdd8e5
Jorn Baayen <jbaayen@src.gnome.org>
2004-05-06 14:23:17 +0000

    ups

--- src/About.cs
+++ src/About.cs
@@ -22,7 +22,7 @@ using Gdk;
 
 public class About
 {
-	private static string version = "0.6";
+	private static string version = "0.6.0";
 	public static string Version {
 		get {
 			return version;
