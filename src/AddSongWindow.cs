c94ad300f56a4bc9dcf5f2fde5198522021e6e6e
Tamara Roberson <tamara.roberson@gmail.com>
2006-09-09 23:12:51 +0000

    Cleanup.
    2006-09-09  Tamara Roberson  <tamara.roberson@gmail.com>
    
    	* src/AddSongWindow.cs: Cleanup.

--- src/AddSongWindow.cs
+++ src/AddSongWindow.cs
@@ -19,10 +19,6 @@
 
 using System;
 using System.Collections;
-
-using Gtk;
-using GLib;
-
 using Mono.Unix;
 
 namespace Muine
@@ -30,172 +26,212 @@ namespace Muine
 	public class AddSongWindow : AddWindow
 	{
 		// GConf
+		// GConf :: Width
 		private const string GConfKeyWidth = "/apps/muine/add_song_window/width";
 		private const int GConfDefaultWidth = 500;
 
+		// GConf :: Height
 		private const string GConfKeyHeight = "/apps/muine/add_song_window/height";
 		private const int GConfDefaultHeight = 475;  
+
 
 		// Strings
 		private static readonly string string_title = 
 			Catalog.GetString ("Play Song");
 
+
 		// Static
 		// Static :: Objects
 		// Static :: Objects :: DnD targets
-		private static TargetEntry [] source_entries = {
+		private static Gtk.TargetEntry [] source_entries = {
 			DndUtils.TargetMuineSongList,
 			DndUtils.TargetUriList
 		};
 
+
 		// Constructor
-		/// <summary>
-		///	Creates a new Add Song window.
-		/// </summary>
-		/// <remarks>
-		///	This is created when "Play Song" is clicked.
-		/// </remarks>
+		/// <summary>Creates a new Add Song window.</summary>
+		/// <remarks>This is created when "Play Song" is clicked.</remarks>
 		public AddSongWindow ()
 		{
 			base.Title = string_title;
 
-			base.SetGConfSize (GConfKeyWidth, GConfDefaultWidth, 
-				GConfKeyHeight, GConfDefaultHeight);
+			base.SetGConfSize
+			  (GConfKeyWidth , GConfDefaultWidth,
+			   GConfKeyHeight, GConfDefaultHeight);
 
 			base.Items = Global.DB.Songs.Values;
 						
 			base.List.Model.SortFunc = new HandleModel.CompareFunc (SortFunc);
 
-			TreeViewColumn col = new TreeViewColumn ();
-			col.Sizing = TreeViewColumnSizing.Fixed;
+			// Column
+			Gtk.TreeViewColumn col = new Gtk.TreeViewColumn ();
+			col.Sizing = Gtk.TreeViewColumnSizing.Fixed;
 			col.PackStart (base.TextRenderer, true);
 			
-			TreeCellDataFunc func = new TreeCellDataFunc (CellDataFunc);
+			Gtk.TreeCellDataFunc func =
+			  new Gtk.TreeCellDataFunc (CellDataFunc);
+
 			col.SetCellDataFunc (base.TextRenderer, func);
+
 			base.List.AppendColumn (col);
-			
+
+			// Setup drag and drop			
 			base.List.DragSource = source_entries;
 			base.List.DragDataGet += OnDragDataGet;
 
+			// Setup handlers
 			Global.DB.SongAdded   += base.OnAdded;
 			Global.DB.SongChanged += base.OnChanged;
 			Global.DB.SongRemoved += base.OnRemoved;
 		}
 
+
+		// Methods
+		// Methods :: Private
+		// Methods :: Private :: GetArtists
+		private string GetArtists (Song song)
+		{
+			string tmp = StringUtils.JoinHumanReadable (song.Artists);
+			return StringUtils.EscapeForPango (tmp);
+		}
+
+		// Methods :: Private :: GetSong
+		private Song GetSong (Gtk.TreeIter iter)
+		{
+			IntPtr ptr = base.List.Model.HandleFromIter (iter);
+			return GetSong (ptr);
+		}
+
+		private Song GetSong (int ptr_i)
+		{
+			IntPtr ptr = new IntPtr (ptr_i);
+			return GetSong (ptr);
+		}
+
+		private Song GetSong (IntPtr ptr)
+		{
+			return Song.FromHandle (ptr);
+		}
+
+		// Methods :: Private :: GetTitle
+		private string GetTitle (Song song)
+		{
+			return StringUtils.EscapeForPango (song.Title);
+		}
+
+		// Methods :: Private :: SetText
+		private void SetText (Gtk.CellRendererText cell, Gtk.TreeIter iter)
+		{			
+			// Song
+			Song song = GetSong (iter);
+			
+			// Info
+			string title   = GetTitle   (song);
+			string artists = GetArtists (song);
+
+			// Format
+			string fmt = "<b>{0}</b>\n{1}";
+			cell.Markup = String.Format (fmt, title, artists);
+		}
+
+		// Methods :: Private :: ProcessDragDataGetSongList
+		private void ProcessDragDataGetSongList
+		  (GLib.List songs, Gtk.DragDataGetArgs args)
+		{
+			string target = DndUtils.TargetMuineSongList.Target;
+	
+			string data = String.Format ("\t{0}\t", target);
+			
+			foreach (int ptr_i in songs) {
+				IntPtr ptr = new IntPtr (ptr_i);
+				string ptr_s = ptr.ToString ();
+				data += (ptr_s + "\r\n");
+			}
+			
+			SetSelectionData (target, data, args);
+		}
+
+		// Methods :: Private :: ProcessDragDataGetUriList
+		private void ProcessDragDataGetUriList
+		  (GLib.List songs, Gtk.DragDataGetArgs args)
+		{
+			string data = "";
+
+			foreach (int song_ptr_i in songs) {
+				Song song = GetSong (song_ptr_i);
+				string uri = FileUtils.UriFromLocalPath (song.Filename);
+				data += (uri + "\r\n");
+			}
+
+			SetSelectionData (DndUtils.TargetUriList.Target, data, args);
+		}
+
+		// Methods :: Private :: SetSelectionData
+		private void SetSelectionData
+		  (string target, string data, Gtk.DragDataGetArgs args)
+		{
+			Gdk.Atom atom = Gdk.Atom.Intern (target, false);
+			byte [] bytes = System.Text.Encoding.ASCII.GetBytes (data); 
+			args.SelectionData.Set (atom, 8, bytes);
+		}
+
+
 		// Handlers
 		// Handlers :: OnDragDataGet (Gtk.DragDataGetHandler)
-		/// <summary>
-		/// 	Handler to be activated when Drag-and-Drop data is requested.
-		/// </summary>
-		/// <remarks>
-		/// 	Songs may be copied by dragging them to Nautilus.
-		/// </remarks>
-		private void OnDragDataGet (object o, DragDataGetArgs args)
+		/// <summary>Handler to be activated when Drag-and-Drop data is
+		///   requested.</summary>
+		/// <remarks>Songs may be copied by dragging them to
+		///   Nautilus.</remarks>
+		private void OnDragDataGet (object o, Gtk.DragDataGetArgs args)
 		{
-			List songs = base.List.SelectedHandles;
-
-			string target;
-			Gdk.Atom atom;
-			byte [] bytes;
+			GLib.List songs = base.List.SelectedHandles;
 
 			switch (args.Info) {
+
+			// Uri list
 			case (uint) DndUtils.TargetType.UriList:
-				string files = "";
-
-				foreach (int p in songs) {
-					IntPtr s = new IntPtr (p);
-					Song song = Song.FromHandle (s);
-					string uri = FileUtils.UriFromLocalPath (song.Filename);
-					files += (uri + "\r\n");
-				}
-
-				target = DndUtils.TargetUriList.Target;
-				atom = Gdk.Atom.Intern (target, false);
-				bytes = System.Text.Encoding.UTF8.GetBytes (files); 
-				args.SelectionData.Set (atom, 8, bytes);
-							
-				break;	
-				
-			case (uint) DndUtils.TargetType.SongList:
-				target = DndUtils.TargetMuineSongList.Target;
+				ProcessDragDataGetUriList (songs, args);
+				break;
 			
-				string ptrs = String.Format ("\t{0}\t", target);
-				
-				foreach (int p in songs) {
-					IntPtr s = new IntPtr (p);
-					string s_s = s.ToString ();
-					ptrs += (s_s + "\r\n");
-				}
-				
-				atom = Gdk.Atom.Intern (target, false);
-				bytes = System.Text.Encoding.ASCII.GetBytes (ptrs);
-				args.SelectionData.Set (atom, 8, bytes);
-
+			// Song list
+			case (uint) DndUtils.TargetType.SongList:
+				ProcessDragDataGetSongList (songs, args);
 				break;
 
+			// Default
 			default:
 				break;	
 			}
 		}
 
+
 		// Delegate Functions
 		// Delegate Functions :: SortFunc
-		/// <summary>
-		/// 	Delegate used in sorting the song list.
-		/// </summary>		
-		/// <param name="a_ptr">
-		///	Handler for first <see cref="Song" />.
-		/// </param>
-		/// <param name="b_ptr">
-		///	Handler for second <see cref="Song" />.
-		/// </param>
-		/// <returns>
-		///	The result of comparing the songs with
-		///	<see cref="Item.CompareTo" />.
-		/// </returns>
+		/// <summary>Delegate used in sorting the song list.</summary>		
+		/// <param name="a_ptr">Handler for first
+		///   <see cref="Song" />.</param>
+		/// <param name="b_ptr">Handler for second
+		///   <see cref="Song" />.</param>
+		/// <returns>The result of comparing the songs with
+		///   <see cref="Item.CompareTo" />.</returns>
 		/// <seealso cref="Item.CompareTo" />
 		private int SortFunc (IntPtr a_ptr, IntPtr b_ptr)
 		{
-			Song a = Song.FromHandle (a_ptr);
-			Song b = Song.FromHandle (b_ptr);
+			Song a = GetSong (a_ptr);
+			Song b = GetSong (b_ptr);
 
 			return a.CompareTo (b);
 		}
 
 		// Delegate Functions :: CellDataFunc
-		/// <summary>
-		/// 	Delegate used to render the song text.
-		/// </summary>		
-		/// <param name="col">
-		///	A <see cref="Gtk.TreeViewColumn" />.
-		/// </param>
-		/// <param name="cell">
-		///	A <see cref="Gtk.CellRenderer" />.
-		/// </param>
-		/// <param name="model">
-		///	A <see cref="Gtk.TreeModel" />.
-		/// </param>
-		/// <param name="iter">
-		///	A <see cref="Gtk.TreeIter" />.
-		/// </param>
+		/// <summary>Delegate used to render the song text.</summary>
 		private void CellDataFunc
-		  (TreeViewColumn col, CellRenderer cell, TreeModel model,
-		   TreeIter iter)
+		  (Gtk.TreeViewColumn col, Gtk.CellRenderer cell, Gtk.TreeModel model,
+		   Gtk.TreeIter iter)
 		{
-			CellRendererText r = (CellRendererText) cell;
-			IntPtr handle = base.List.Model.HandleFromIter (iter);
-			Song song = Song.FromHandle (handle);
-
-			string fmt = "<b>{0}</b>\n{1}";
-
-			string title = StringUtils.EscapeForPango (song.Title);
-			
-			string artists_tmp =
-			  StringUtils.JoinHumanReadable (song.Artists);
-			
-			string artists = StringUtils.EscapeForPango (artists_tmp);
-
-			r.Markup = String.Format (fmt, title, artists);
-		}
+			Gtk.CellRendererText cell_txt = (Gtk.CellRendererText) cell;
+			SetText (cell_txt, iter);
+		}		
 	}
 }
