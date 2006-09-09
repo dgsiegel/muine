67c3abcec5a7e62d5547f7eda679872989afc5fc
Tamara Roberson <tamara.roberson@gmail.com>
2006-09-09 21:54:22 +0000

    Cleanup.
    2006-09-09  Tamara Roberson  <tamara.roberson@gmail.com>
    
    	* src/AddAlbumWindow.cs: Cleanup.

--- src/AddAlbumWindow.cs
+++ src/AddAlbumWindow.cs
@@ -19,10 +19,6 @@
 
 using System;
 using System.Collections;
-
-using Gtk;
-using GLib;
-
 using Mono.Unix;
 
 namespace Muine
@@ -38,6 +34,7 @@ namespace Muine
 		private const string GConfKeyHeight = "/apps/muine/add_album_window/height";
 		private const int GConfDefaultHeight = 475; 
 
+
 		// Strings
 		private static readonly string string_title = 
 			Catalog.GetString ("Play Album");
@@ -45,288 +42,331 @@ namespace Muine
 		private static readonly string string_artists = 
 			Catalog.GetString ("Performed by {0}");
 
+
 		// Static
 		// Static :: Objects
 		// Static :: Objects :: DnD Targets
-		private static TargetEntry [] source_entries = {
+		private static Gtk.TargetEntry [] source_entries = {
 			DndUtils.TargetMuineAlbumList,
 			DndUtils.TargetUriList
 		};
 
+
 		// Widgets
-		private CellRenderer pixbuf_renderer = new CellRendererPixbuf ();
+		private Gtk.CellRenderer pixbuf_renderer =
+		  new Gtk.CellRendererPixbuf ();
 
 		private Gdk.Pixbuf nothing_pixbuf =
 		  new Gdk.Pixbuf (null, "muine-nothing.png");
+
 
 		// Variables
 		private bool drag_dest_enabled = false;
 		private int pixbuf_column_width = CoverDatabase.CoverSize + (5 * 2);
 
+
 		// Constructor
-		/// <summary>
-		///	Creates a new Add Album window.
-		/// </summary>
-		/// <remarks>
-		///	This is created when "Play Album" is clicked.
-		/// </remarks>
+		/// <summary>Creates a new Add Album window.</summary>
+		/// <remarks>This is created when "Play Album" is clicked.</remarks>
 		public AddAlbumWindow ()
 		{
 			base.Title = string_title;
 
-			base.SetGConfSize (GConfKeyWidth , GConfDefaultWidth, 
-					   GConfKeyHeight, GConfDefaultHeight);
+			base.SetGConfSize
+			  (GConfKeyWidth , GConfDefaultWidth,
+			   GConfKeyHeight, GConfDefaultHeight);
 
 			base.Items = Global.DB.Albums.Values;
 						
 			base.List.Model.SortFunc = new HandleModel.CompareFunc (SortFunc);
 
 			// Column
-			TreeViewColumn col = new TreeViewColumn ();
-			col.Sizing = TreeViewColumnSizing.Fixed;
+			Gtk.TreeViewColumn col = new Gtk.TreeViewColumn ();
+			col.Sizing = Gtk.TreeViewColumnSizing.Fixed;
 			col.Spacing = 4;
 
 			col.PackStart (pixbuf_renderer  , false);
 			col.PackStart (base.TextRenderer, true );
-			
-			TreeCellDataFunc func1 = new TreeCellDataFunc (PixbufCellDataFunc);
-			TreeCellDataFunc func2 = new TreeCellDataFunc (TextCellDataFunc  );
-			
-			col.SetCellDataFunc (pixbuf_renderer  , func1);
-			col.SetCellDataFunc (base.TextRenderer, func2);
 
+			// Data functions
+			Gtk.TreeCellDataFunc func;
+			
+			func = new Gtk.TreeCellDataFunc (PixbufCellDataFunc);
+			col.SetCellDataFunc (pixbuf_renderer, func);
+
+			func = new Gtk.TreeCellDataFunc (TextCellDataFunc);			
+			col.SetCellDataFunc (base.TextRenderer, func);
+
+			// Add column
 			base.List.AppendColumn (col);
 
+			// Setup drag and drop
 			base.List.DragSource = source_entries;
 			base.List.DragDataGet += OnDragDataGet;
 
+			// Setup handlers
 			Global.DB.AlbumAdded   += base.OnAdded;
 			Global.DB.AlbumChanged += base.OnChanged;
 			Global.DB.AlbumRemoved += base.OnRemoved;
 
 			Global.CoverDB.DoneLoading += OnCoversDoneLoading;
 
+			// Enable drag and drop if we're not busy loading covers.
 			if (!Global.CoverDB.Loading)
 				EnableDragDest ();
 		}
 
+
 		// Methods
 		// Methods :: Private
 		// Methods :: Private :: EnableDragDest
-		/// <summary>
-		/// 	Turns on Drag-and-Drop.
-		/// </summary>
+		/// <summary>Turns on Drag-and-Drop.</summary>
 		private void EnableDragDest ()
 		{
+			// Return if already enabled
 			if (drag_dest_enabled)
 				return;
 
+			// Add handler
 			base.List.DragDataReceived += OnDragDataReceived;
 
-			Gtk.Drag.DestSet (base.List, DestDefaults.All,
-				CoverImage.DragEntries, Gdk.DragAction.Copy);
+			// Gtk settings
+			Gtk.Drag.DestSet
+			  (base.List, Gtk.DestDefaults.All, CoverImage.DragEntries,
+			   Gdk.DragAction.Copy);
 
+			// Mark as enabled
 			drag_dest_enabled = true;
 		}
 
+		// Methods :: Private :: GetAlbum
+		private Album GetAlbum (Gtk.TreeIter iter)
+		{
+			IntPtr ptr = base.List.Model.HandleFromIter (iter);
+			return GetAlbum (ptr);
+		}
+		
+		private Album GetAlbum (int x, int y)
+		{
+			Gtk.TreePath path = null;
+			base.List.GetPathAtPos (x, y, out path);
+
+			if (path == null)
+				return null;
+		
+			IntPtr ptr = base.List.Model.HandleFromPath (path);
+			return GetAlbum (ptr);
+		}
+
+		private Album GetAlbum (int ptr_i)
+		{
+			IntPtr ptr = new IntPtr (ptr_i);
+			return GetAlbum (ptr);
+		}
+
+		private Album GetAlbum (IntPtr ptr)
+		{
+			return Album.FromHandle (ptr);
+		}
+
+		// Methods :: Private :: GetAlbumName
+		private string GetAlbumName (Album album)
+		{
+			return StringUtils.EscapeForPango (album.Name);			
+		}
+
+		// Methods :: Private :: GetArtists
+		private string GetArtists (Album album)
+		{
+			string tmp = StringUtils.JoinHumanReadable (album.Artists, 3);
+			return StringUtils.EscapeForPango (tmp);
+		}
+
+		// Methods :: Private :: GetCoverImage
+		private Gdk.Pixbuf GetCoverImage (Album album)
+		{
+			if (album.CoverImage != null)
+				return album.CoverImage;
+
+			if (Global.CoverDB.Loading)
+				return Global.CoverDB.DownloadingPixbuf;
+
+			return nothing_pixbuf;			
+		}
+
+		// Methods :: Private :: GetPerformers
+		private string GetPerformers (Album album)
+		{			
+			if (album.Performers.Length < 1)
+				return String.Empty;
+			
+			string tmp = StringUtils.JoinHumanReadable (album.Performers, 2);
+			string tmp2 = String.Format (string_artists, tmp);
+			return StringUtils.EscapeForPango (tmp2);
+		}
+
+		// Methods :: Private :: ProcessDragDataGetUriList
+		private void ProcessDragDataGetUriList
+		  (GLib.List albums, Gtk.DragDataGetArgs args)
+		{
+			string data = "";
+
+			foreach (int album_ptr_i in albums) {
+				Album album = GetAlbum (album_ptr_i);
+
+				foreach (Song song in album.Songs) {
+					string uri = FileUtils.UriFromLocalPath (song.Filename);
+					data += (uri + "\r\n");
+				}
+			}
+
+			SetSelectionData (DndUtils.TargetUriList.Target, data, args);
+		}
+
+		// Methods :: Private :: ProcessDragDataGetAlbumList
+		private void ProcessDragDataGetAlbumList
+		  (GLib.List albums, Gtk.DragDataGetArgs args)
+		{
+			string target = DndUtils.TargetMuineAlbumList.Target;
+		
+			string data = String.Format ("\t{0}\t", target);
+			
+			foreach (int ptr_i in albums) {
+				IntPtr ptr = new IntPtr (ptr_i);
+				string ptr_s = ptr.ToString ();
+				data += (ptr_s + "\r\n");
+			}
+			
+			SetSelectionData (target, data, args);
+		}
+
+		// Methods :: Private :: SetCoverImage
+		private void SetCoverImage
+		  (Gtk.CellRendererPixbuf cell, Gtk.TreeIter iter)
+		{			
+			Album album = GetAlbum (iter);
+			cell.Pixbuf = GetCoverImage (album);
+			cell.Width = cell.Height = pixbuf_column_width;
+		}
+
+		// Methods :: Private :: SetText
+		private void SetText (Gtk.CellRendererText cell, Gtk.TreeIter iter)
+		{			
+			// Album
+			Album album = GetAlbum (iter);
+			
+			// Info
+			string name       = GetAlbumName  (album);
+			string performers = GetPerformers (album);
+			string artists    = GetArtists    (album);
+
+			// Format
+			string fmt = "<b>{0}</b>\n{1}\n\n{2}";
+			cell.Markup = String.Format (fmt, name, artists, performers);
+		}
+
+
+		// Methods :: Private :: SetSelectionData
+		private void SetSelectionData
+		  (string target, string data, Gtk.DragDataGetArgs args)
+		{
+			Gdk.Atom atom = Gdk.Atom.Intern (target, false);
+			byte [] bytes = System.Text.Encoding.ASCII.GetBytes (data); 
+			args.SelectionData.Set (atom, 8, bytes);
+		}
+
+
 		// Handlers
 		// Handlers :: OnCoversDoneLoading
-		/// <csummary>
-		/// 	Handler called when the album covers are done loading.
-		/// </summary>
-		/// <remarks>
-		///	Enables Drag-and-Drop and redraws the list.
-		/// </remarks>
+		/// <summary>Handler called when the album covers are done
+		///   loading.</summary>
+		/// <remarks>Enables Drag-and-Drop and redraws the list.</remarks>
 		private void OnCoversDoneLoading ()
 		{
 			EnableDragDest ();
-
 			base.List.QueueDraw ();
 		}
 
-		// Handlers :: OnDragDataReceived
-		/// <summary>
-		/// 	Handler called when Drag-and-Drop data is received.
-		/// </summary>
-		/// <remarks>
-		/// 	External covers may be Drag-and-Dropped onto an album.
-		/// </remarks>
-		private void OnDragDataReceived (object o, DragDataReceivedArgs args)
-		{
-			TreePath path;
-			if (!base.List.GetPathAtPos (args.X, args.Y, out path))
-				return;
-
-			IntPtr album_ptr = base.List.Model.HandleFromPath (path);
-			Album album = Album.FromHandle (album_ptr);
-
-			CoverImage.HandleDrop ((Song) album.Songs [0], args);
-		}
-
 		// Handlers :: OnDragDataGet
-		/// <summary>
-		/// 	Handler to be activated when Drag-and-Drop data is requested.
-		/// </summary>
-		/// <remarks>
-		/// 	Albums may be copied by dragging them to Nautilus.
-		/// </remarks>
-		private void OnDragDataGet (object o, DragDataGetArgs args)
+		/// <summary>Handler to be activated when Drag-and-Drop data is
+		///   requested.</summary>
+		/// <remarks>Albums may be copied by dragging them to
+		///   Nautilus.</remarks>
+		private void OnDragDataGet
+		  (object o, Gtk.DragDataGetArgs args)
 		{
-			List albums = base.List.SelectedHandles;
-			
-			string target;
-			Gdk.Atom atom;
-			byte [] bytes;
+			GLib.List albums = base.List.SelectedHandles;
 
 			switch (args.Info) {
+
+			// Uri list
 			case (uint) DndUtils.TargetType.UriList:
-				string files = "";
-
-				foreach (int i in albums) {
-					IntPtr p = new IntPtr (i);
-					Album a = Album.FromHandle (p);
-
-					foreach (Song s in a.Songs) {
-						string uri = FileUtils.UriFromLocalPath (s.Filename);
-						files += (uri + "\r\n");
-					}
-				}
-
-				target = DndUtils.TargetUriList.Target;
-				atom = Gdk.Atom.Intern (target, false);
-				bytes = System.Text.Encoding.UTF8.GetBytes (files);
-				args.SelectionData.Set (atom, 8, bytes);
-
+				ProcessDragDataGetUriList (albums, args);
 				break;
 
+			// Album list
 			case (uint) DndUtils.TargetType.AlbumList:
-				target = DndUtils.TargetMuineAlbumList.Target;
-			
-				string ptrs = String.Format ("\t{0}\t", target);
-				
-				foreach (int p in albums) {
-					IntPtr s = new IntPtr (p);
-					string s_s = s.ToString ();
-					ptrs += s_s + "\r\n";
-				}
-				
-				atom = Gdk.Atom.Intern (target, false);
-				bytes = System.Text.Encoding.ASCII.GetBytes (ptrs); 
-				args.SelectionData.Set (atom, 8, bytes);
-							
+				ProcessDragDataGetAlbumList (albums, args);
 				break;
 
+			// Default
 			default:
 				break;	
 			}
 		}
 
+		// Handlers :: OnDragDataReceived
+		/// <summary>Handler called when Drag-and-Drop data is
+		///   received.</summary>
+		/// <remarks>External covers may be Drag-and-Dropped onto an
+		///   album.</remarks>
+		private void OnDragDataReceived
+		  (object o, Gtk.DragDataReceivedArgs args)
+		{
+			Album album = GetAlbum (args.X, args.Y);
+			Song song = (Song) album.Songs [0];
+			CoverImage.HandleDrop (song, args);
+		}
+
+
 		// Delegate Functions		
 		// Delegate Functions :: SortFunc
-		/// <summary>
-		/// 	Delegate used in sorting the album list.
-		/// </summary>
-		/// <param name="a_ptr">
-		///	Handler for first <see cref="Album" />.
-		/// </param>
-		/// <param name="b_ptr">
-		///	Handler for second <see cref="Album" />.
-		/// </param>
-		/// <returns>
-		///	The result of comparing the albums with
-		///	<see cref="Item.CompareTo" />.
-		/// </returns>
+		/// <summary>Delegate used in sorting the album list.</summary>
+		/// <param name="a_ptr">Handler for first
+		///   <see cref="Album" />.</param>
+		/// <param name="b_ptr">Handler for second
+		///   <see cref="Album" />.</param>
+		/// <returns>The result of comparing the albums with
+		///   <see cref="Item.CompareTo" />.</returns>
 		/// <seealso cref="Item.CompareTo" />
 		private int SortFunc (IntPtr a_ptr, IntPtr b_ptr)
 		{
-			Album a = Album.FromHandle (a_ptr);
-			Album b = Album.FromHandle (b_ptr);
+			Album a = GetAlbum (a_ptr);
+			Album b = GetAlbum (b_ptr);
 
 			return a.CompareTo (b);
 		}
 
-		// Delegate Functions :: PixbufCellDataFunc
-		/// <summary>
-		/// 	Delegate used to render the covers.
-		/// </summary>
-		/// <param name="col">
-		///	A <see cref="Gtk.TreeViewColumn" />.
-		/// </param>
-		/// <param name="cell">
-		///	A <see cref="Gtk.CellRenderer" />.
-		/// </param>
-		/// <param name="model">
-		///	A <see cref="Gtk.TreeModel" />.
-		/// </param>
-		/// <param name="iter">
-		///	A <see cref="Gtk.TreeIter" />.
-		/// </param>
+		// Delegate Functions :: Data Function
+		// Delegate Functions :: Data Function :: PixbufCellDataFunc
+		/// <summary>Delegate used to render the covers.</summary>
 		private void PixbufCellDataFunc
-		  (TreeViewColumn col, CellRenderer cell, TreeModel model,
-		   TreeIter iter)
+		  (Gtk.TreeViewColumn col, Gtk.CellRenderer cell, Gtk.TreeModel model,
+		   Gtk.TreeIter iter)
 		{
-			CellRendererPixbuf r = (CellRendererPixbuf) cell;
-			
-			IntPtr handle = base.List.Model.HandleFromIter (iter);
-			Album album = Album.FromHandle (handle);
-
-			if (album.CoverImage != null)
-				r.Pixbuf = album.CoverImage;
-
-			else if (Global.CoverDB.Loading)
-				r.Pixbuf = Global.CoverDB.DownloadingPixbuf;
-
-			else
-				r.Pixbuf = nothing_pixbuf;
-
-			r.Width = r.Height = pixbuf_column_width;
+			Gtk.CellRendererPixbuf cell_pb = (Gtk.CellRendererPixbuf) cell;
+			SetCoverImage (cell_pb, iter);
 		}
+	
 
-		// Delegate Functions :: TextCellDataFunc
-		/// <summary>
-		/// 	Delegate used to render the album text.
-		/// </summary>		
-		/// <param name="col">
-		///	A <see cref="Gtk.TreeViewColumn" />.
-		/// </param>
-		/// <param name="cell">
-		///	A <see cref="Gtk.CellRenderer" />.
-		/// </param>
-		/// <param name="model">
-		///	A <see cref="Gtk.TreeModel" />.
-		/// </param>
-		/// <param name="iter">
-		///	A <see cref="Gtk.TreeIter" />.
-		/// </param>
+		// Delegate Functions :: Data Function :: TextCellDataFunc
+		/// <summary>Delegate used to render the album text.</summary>		
 		private void TextCellDataFunc
-		  (TreeViewColumn col, CellRenderer cell, TreeModel model,
-		   TreeIter iter)
+		  (Gtk.TreeViewColumn col, Gtk.CellRenderer cell, Gtk.TreeModel model,
+		   Gtk.TreeIter iter)
 		{
-			CellRendererText r = (CellRendererText) cell;
-			
-			IntPtr handle = base.List.Model.HandleFromIter (iter);
-			Album album = Album.FromHandle (handle);
-
-			string performers = "";
-			if (album.Performers.Length > 0) {
-				string performers_tmp =
-				  StringUtils.JoinHumanReadable (album.Performers, 2);
-			
-				string performers_tmp2 =
-				  String.Format (string_artists, performers_tmp);
-			
-				performers = StringUtils.EscapeForPango (performers_tmp2);
-			}
-
-			string fmt = "<b>{0}</b>\n{1}\n\n{2}";
-			string album_s = StringUtils.EscapeForPango (album.Name);
-
-			string artists_tmp =
-			  StringUtils.JoinHumanReadable (album.Artists, 3);
-			  
-			string artists = StringUtils.EscapeForPango (artists_tmp);
-
-			r.Markup = String.Format (fmt, album_s, artists, performers);
-		}
+			Gtk.CellRendererText cell_txt = (Gtk.CellRendererText) cell;
+			SetText (cell_txt, iter);
+		}		
 	}
 }
